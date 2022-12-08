517cbd09b184ea0ea62340967fe4346586f08e5f
itsmaruf <maruf@corexlab.dev>
2022-12-08 12:10:25 +0600

    contact route done

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
-var contactsRouter = require("./routes/contacts");
+var contactsRouter = require("./routes/contactLists");
 
 var app = express();
 
--- routes/contactLists.js
+++ routes/contactLists.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET contact listing. */
 router.get("/", function (req, res, next) {
-  res.send("fetching contacts list");
+  const contactList = [
+    {
+      id: 1,
+      name: "John Doe",
+      phone: "555-555-5555",
+    },
+  ];
+  res.send(contactList);
 });
 
 module.exports = router;
